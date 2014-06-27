6a8401c87cbbeed191964e14a08768dfa4c6cb46
Moises Arcos <marcos@emergya.com>
2014-06-27 12:15:44 +0200

    [#104489] Add an example to load a layer tree from json variable

---
+++ examples/add_tree_to_layer_tree.js
@@ -0,0 +1,116 @@
+function initMap() {
+	var map = SMC.map('map');
+	map.setView([37.383333, -4.983333], 8);
+	var base = SMC.tileLayer('http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="http://cloudmade.com">CloudMade</a>',
+		maxZoom: 18
+	}).addTo(map);
+    var satelite = SMC.wmsLayer("http://www.idee.es/wms/PNOA/PNOA", {
+        layers: "PNOA",
+        format: 'image/png',
+        transparent: true,
+        crs: L.CRS.EPSG4326,
+        attribution: "Map data &copy; Instituto Geográfico Nacional de España"
+    });
+    var baseLayer = {
+        "Street Map": base,
+        "Satelite": satelite
+    };
+    var leyenda = SMC.layerTreeControl(baseLayer, {
+        collapsed: false
+    }).addTo(map);
+
+    var tree = [{
+        type: "folder",
+        label: 'REDIAM',
+        layers: [{
+            type: "folder",
+            label: "Paisajes",
+            layers: [{
+                type: "SMC.layers.WMSLayer",
+                url: "http://www.juntadeandalucia.es/medioambiente/mapwms/REDIAM_mapa_paisaje_andalucia",
+                params: [{
+                    layers: "categ_paisaj",
+                    format: 'image/png',
+                    transparent: true,
+                    crs: L.CRS.EPSG4326,
+                    attribution: "Map data &copy; REDIAM",
+                    label: "Categorias paisajisticas"
+                }] 
+            },{
+                type: "SMC.layers.WMSLayer",
+                url: "http://www.juntadeandalucia.es/medioambiente/mapwms/REDIAM_mapa_paisaje_andalucia",
+                params: [{
+                    layers: "amb_paisaj",
+                    format: 'image/png',
+                    transparent: true,
+                    crs: L.CRS.EPSG4326,
+                    attribution: "Map data &copy; REDIAM",
+                    label: "Ambitos paisajisticos"
+                }] 
+            },{
+                type: "SMC.layers.WMSLayer",
+                url: "http://www.juntadeandalucia.es/medioambiente/mapwms/REDIAM_mapa_paisaje_andalucia",
+                params: [{
+                    layers: "areaspai",
+                    format: 'image/png',
+                    transparent: true,
+                    crs: L.CRS.EPSG4326,
+                    attribution: "Map data &copy; REDIAM",
+                    label: "Areas paisajisticas"
+                }] 
+            }]
+        },{
+            type: "folder",
+            label: "Vias Pecuarias",
+            layers: [{
+                type: "SMC.layers.WMSLayer",
+                url: "http://www.juntadeandalucia.es/medioambiente/mapwms/REDIAM_Inventario_VVPP",
+                params: [{
+                    layers: "linea_base_deslindada",
+                    format: 'image/png',
+                    transparent: true,
+                    crs: L.CRS.EPSG4326,
+                    attribution: "Map data &copy; REDIAM",
+                    label: "Linea Base Deslindada"
+                }] 
+            },{
+                type: "SMC.layers.WMSLayer",
+                url: "http://www.juntadeandalucia.es/medioambiente/mapwms/REDIAM_Inventario_VVPP",
+                params: [{
+                    layers: "Lugares_VVPP",
+                    format: 'image/png',
+                    transparent: true,
+                    crs: L.CRS.EPSG4326,
+                    attribution: "Map data &copy; REDIAM",
+                    label: "Lugares VVPP"
+                }] 
+            },{
+                type: "SMC.layers.WMSLayer",
+                url: "http://www.juntadeandalucia.es/medioambiente/mapwms/REDIAM_Inventario_VVPP",
+                params: [{
+                    layers: "Inventario_VVPP",
+                    format: 'image/png',
+                    transparent: true,
+                    crs: L.CRS.EPSG4326,
+                    attribution: "Map data &copy; REDIAM",
+                    label: "Inventario VVPP"
+                }] 
+            }]
+        }]
+    },{
+        type: "folder",
+        label: "Capas WFS",
+        layers: [{
+            type: "SMC.layers.markers.WFSMarkerLayer",
+            params: [{
+                serverURL: "http://www.ideandalucia.es/dea100/wfs",
+                typeName: "ideandalucia:it03_aeropuerto_pun",
+                label: "Prueba WFS",
+                outputFormat: "json"
+            }]
+        }]
+    }];
+    map.loadLayers(tree);
+}
+window.onload = initMap;
